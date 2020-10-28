78ff048a8c049d64bb256bad7586090dabb03f8b
Rebecca Blanton <beccaablanton@gmail.com>
2020-10-27 17:40:35 -0700

    added README with information about site. added img file of website. need to finish local storage function. erroring

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # yourworkday
+
+## What it is
+
+YourWorkDay is a simple day planner interface to write all your todos of the day from 9 am to 6 pm. As the day goes on each hour block will go from grey to let you know its passed, your current hour will be displayed red to let you know what to do that hour and green for the rest of the hours coming up.
+
+## Components
+
+* current day, date and time of day displayed in header for easy tracking.
+* time blocks for each hour of a typical work day from 9 am to 6 pm.
+** time blocks will change color depending on current time of day.
+* editable time blocks, just press save to the button to the right and your data will be saved even when you refresh.
+
+## Website Example and Link
+
+[Go to Live YourWorkday Page](https://beccablanton.github.io/yourworkday/)
+
+![Website snapshot](assets/yourworkdaywebsiteexample.png)
--- workday.js
+++ workday.js
@@ -1,13 +1,3 @@
-/*
-WHEN I click into a time block
-THEN I can enter an event
-WHEN I click the save button for that time block
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-add event listeners to create events within boxes
-save text entered in local storage to make sure information is saved when refreshing*/
-
 
 //variables and function for displaying Date and Time in Header
 const currentDay = document.getElementById("currentDay")
@@ -21,30 +11,44 @@ updateTime();
 //Function to pull data hour from each time block to assign hour compared to current hour of day and change the background of each time block
 var currentHour = moment().format('H')
 console.log(currentHour)
+
     $(".time-block").each(function() {
         if(parseInt($(this).data("hour")) < currentHour){
             $(this).addClass("past")
-            } else if (parseInt($(this).data("hour")) === currentHour){
+            } else if (parseInt($(this).data("hour")) == currentHour){
                 $(this).addClass("present")
             } else {
                 $(this).addClass("future")
             }
-            console.log(currentHour)
             console.log(parseInt($(this).data("hour")))
-            console.log($(this).data("hour"))
         })
-
+        
 //making each button link to text box and upload onto local storage
-$("#btn1").on("click", function(event) {
-    alert("I've been clicked!");
-    event.preventDefault();
-    localStorage.setItem("textBox1", JSON.stringify(textBox1));
-    var eventCreated1 = JSON.parse(localStorage.getitem(textBox1));
-    textBox1.textContent = eventCreated1
-    console.log(event)
-    console.log(textBox1)
-    console.log(eventCreated1)
-  });
+/*$('button').each.on("click", function(event){
+localStorage.setItem(this.id,$('form').val())
+})*/
+
+$('form').each(function(el){
+    console.log(this)
+    $(this).find('input[type="text"]').val(inputValues[this.id])
+    console.log(this.id)
+})
+
+  $('form').on('submit'), function(e){
+      e.preventDefault()
+      var item= this.id
+      var val = $(this).find('inputs').val()
+      inputValues[item] = val
+        localStorage.setItem('inputs', JSON.stringify(inputValues))
+      
+  }
+  var inputValues = JSON.parse(localStorage.getitem("inputs"));
+  console.log("inputs")
+
+
+  //localStorage.setItem(this.id, $(this).find('input').val() )
+    //  $(this).find("input").val
+      //console.log(this)
 /*document.getElementById("btn1").addEventListener("click", function(event){
     event.preventDefault();
     localStorage.setItem("textBox1", JSON.stringify(textBox1));
@@ -53,7 +57,15 @@ $("#btn1").on("click", function(event) {
     console.log(event)
     console.log(textBox1)
     console.log(eventCreated1)
-
+$("#btn1").on("", function(event) {
+    alert("I've been clicked!");
+    event.preventDefault();
+    localStorage.setItem("textBox1", JSON.stringify(textBox1));
+    
+    textBox1.textContent = eventCreated1
+    console.log(event)
+    console.log(textBox1)
+    console.log(eventCreated1)
 })
 //save text to localstorage.
 
